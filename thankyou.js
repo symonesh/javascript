14aa2aa82d616c3ad3d17fba6a4f51b36fd6c3af
Saurav Ban <84563943+symonesh@users.noreply.github.com>
2022-12-18 17:42:07 +0545

    Create thankyou.js

---
+++ thankyou.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import WelcomeMessage from './WelcomeMessage';
+
+function App() {
+  return (
+    <div>
+      <WelcomeMessage />
+      <p>Thank you for visiting our website!</p>
+    </div>
+  );
+}
+
+export default App;
